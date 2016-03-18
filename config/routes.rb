e04d2e14579f08ace768fe2ed19fa2cd981d4cea
Paweł Ropa <pawel.ropa@gmail.com>
2016-03-18 07:50:07 +0100

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
-  get 'users/new'
-
   root             'static_pages#home'
   
   get 'help'    => 'static_pages#help'
@@ -12,7 +6,11 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup'  => 'users#new'
-  
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
